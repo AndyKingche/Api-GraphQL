2c4288f92e07ad977abfdb44f19375be052a626b
Andykingche <laqm_14@hotmail.com>
2020-12-29 13:36:54 -0500

    Ponerla en produccion

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { graphqlHTTP } = require('express-graphql')
 const { readFile, readFileSync } = require('fs')
 const { join } = require('path')
 const resolvers = require('./lib/resolver')
+const cors = require('cors')
 const app = express()
 const port = process.env.port || 3000
 
@@ -14,6 +15,7 @@ const typeDefs = readFileSync(
         __dirname,'lib','schema.graphql'),'utf-8')
 const schema = makeExecutableSchema({typeDefs,resolvers})
 
+app.use(cors())
 app.use('/api', graphqlHTTP({
   schema: schema,
   rootValue: resolvers,
